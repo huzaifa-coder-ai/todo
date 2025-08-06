b3dacf9c12b0d895ddb171e6018d7c319871538d
Huzaifa Bilal <huzaifajanjua626@gmail.com>
2025-08-06 15:57:18 +0500

    Updated the todo App

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,36 +10,41 @@ const Card = () => {
         e.preventDefault()
 
         if(inputVal == ''){
-            alert("Add atleast One Task")
+            alert("Add at least one task")
             
         }
         else{
 
             settodos([...todos,inputVal])  
-               
+            setinputVal('')
         }
  
      }
 
-     const deleteBtn = () =>{
+     const deleteBtn = () => {
 
      }
+
 
   return (
     <>
     <h1>ToDo App React JS</h1>
     <form onSubmit={submitHandler}>
         <h2>Todo List</h2>
-        <input
+        <input value={inputVal}
         onChange={(e) => { 
             setinputVal(e.target.value)
          }}
          type="text" placeholder='Write Your Task here...' />
         <button type='submit'>Add Task</button>
-        <h3>My ToDo's :</h3>
+        <div className="flex">
+            <h3>My ToDo's :</h3>
+            <i id='edit' className="fa-solid fa-pen-to-square"></i>
+        </div>
+        
         <ul>
         {todos.map((todo)=>{
-        return  <li>{todo}  <i onClick={deleteBtn} className="fa-solid fa-trash"></i>  </li>
+        return  <li>{todo}  <i onClick={deleteBtn} id='del' className="fa-solid fa-trash"></i>  </li>
         })
             
         }
